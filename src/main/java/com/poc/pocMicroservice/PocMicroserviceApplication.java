3063f5c6d7c2a94e8372bbef33808f38360287fe
Wellington Fernandes <wellingtonfernandes013@gmail.com>
2023-03-28 22:08:10 -0300

    Arquivos iniciais do repo

---
+++ src/main/java/com/poc/pocMicroservice/PocMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.poc.pocMicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocMicroserviceApplication.class, args);
+	}
+
+}
